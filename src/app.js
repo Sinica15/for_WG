81debb9bb71024559505b7412de05d0f29fb317d
Sinica15 <ruja_vl@mail.ru>
2019-02-24 04:17:43 +0300

    fn3rd

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-// this is an example of improting data from JSON
-//import 'orders' from '../data/orders.json';
+var $ = require("jquery");
+
 var orders = require('../data/orders.json');
 var users = require('../data/users.json');
 var companies = require('../data/companies.json');
@@ -9,6 +9,42 @@ import tableRender from "./tableRender.js";
 
 export default (function () {
     var app = document.getElementById("app");
+    
     console.log("1");
+    
     tableRender(app, orders, users, companies);
+    
+    $(".user_data > a").click(function(event) {
+//        console.log(332);
+        event.preventDefault(); 
+        
+    });
+    
+//    $('#block').one('click', fn);
+
+    
+//    $(".user_data > a").one('click', function(){
+//        console.log(228);
+//        $(".user-details").siblings().css('display', 'block');
+//    });
+    
+    
+    $("td.user_data > a").click(function(){
+        if($(this).siblings().css('display') == 'block'){
+            $(this).siblings().css('display', 'none');
+        }else{
+            $(this).siblings().css('display', 'block');
+        }
+    });
+    
+    
+    
+    
+
+    
+    
+
+
+
+
 }());
--- src/tableRender.js
+++ src/tableRender.js
@@ -1,4 +1,90 @@
+const moment = require('moment');
 
+function getNecessaryDateFormat (date, format){
+    return moment(date*1000).format(format);
+}
+
+function getNecessaryCurrency(value ,course){
+    var currencySymbol = "$";
+    return currencySymbol + " " + value*course;
+}
+
+function hideCardNumber(number){
+    var outPutCardNumber = "";
+    for (let i = 0; i < number.length - 4; i++){
+        if (i>1){
+            outPutCardNumber+= "*";
+        }else{
+            outPutCardNumber+= number[i];
+        }
+    }
+    for(let i = 0, j = number.length - 5; i < 4; i++, j++){
+        outPutCardNumber+= number[j];
+    }
+    return outPutCardNumber;
+}
+
+function getUserName(userObject){
+    var sex = 'Ms.';
+    if(userObject['gender'] == 'Male'){
+        sex = 'Mr.';
+    }
+    var userName = sex + " " + userObject['first_name'] + " " + userObject['last_name'];
+    return '<a href="#">' + userName + '</a>';
+}
+
+function getCompanieInfo(id, companies){
+    for (let i = 0; i < companies.length; i++){
+        if(id == companies[i]['id']){
+            return companies[i];
+        }
+    }
+}
+
+function getUserDetails(userObject, companies){
+    var userDetails = "";
+    if(userObject['birthday'] != null){
+        userDetails+= '<p>Birthday: ' + getNecessaryDateFormat(userObject['birthday'], 'MM/DD/YYYY') + '</p>';
+    }
+    if(userObject['avatar'] != null){
+        userDetails+= '<p><img src="' + userObject['avatar'] + '" width="100px"></p>'
+    }
+    if (userObject['company_id'] != null){
+        var companieObject = getCompanieInfo(userObject['company_id'], companies);
+        if ((companieObject['url'] != null)&&(companieObject['title'] != null)){
+            userDetails+='<p>Company: <a href="' + companieObject['url'] + '" target="_blank">' + companieObject['title'] + '</a></p>';
+        }else{
+            if(companieObject['url'] != null){
+                userDetails+='<p>Company: <a href="' + companieObject['url'] + '" target="_blank"> companie site </a></p>';
+            }
+            if(companieObject['title'] != null){
+                userDetails+='<p>Company: ' + companieObject['title'] + '</p>';
+            }
+        }
+        if ((companieObject['industry'] != null)&&(companieObject['sector'] != null)){
+            userDetails+='<p>Industry: ' + companieObject['industry'] + ' / ' + companieObject['sector'] + '</p>';
+        }else{
+            if(companieObject['industry'] != null){
+                userDetails+='<p>Industry: ' + companieObject['industry'];
+            }
+            if(companieObject['sector'] != null){
+                userDetails+='<p>Sector: ' + companieObject['sector'] + '</p>';
+            }
+        }
+        
+    }
+    return '<div class="user-details">' + userDetails + '</div>';
+}
+
+function getUserData(id, users, companies){
+    var userObject;
+    for (let i = 0; i < users.length; i++){
+        if (id == users[i]['id']){
+            userObject = users[i];
+        }
+    }
+    return getUserName(userObject) + getUserDetails(userObject, companies);
+}
 
 export default function tableRender(nodeTo, orders, users, companies) {
     
@@ -8,11 +94,14 @@ export default function tableRender(nodeTo, orders, users, companies) {
     
         
     for(let i = 0; i <orders.length; i++){
-        outPutTable+="<tr>";
-        for (let cell in orders[i]){
-            outPutTable+="<td>" + orders[i][cell] + "<td>";
-        }
-//        outPutTable+= "" +orders[i];
+        outPutTable+="<tr id='" + orders[i]['id'] + "'>";
+        outPutTable+="<td>" + orders[i]['transaction_id'] + "</td>";
+        outPutTable+="<td class='user_data'>" + getUserData(orders[i]['user_id'], users, companies) + "</td>";
+        outPutTable+="<td>" + getNecessaryDateFormat(orders[i]['created_at'], 'MM/DD/YYYY, HH:MM:SS LT') + "</td>";
+        outPutTable+="<td>" + getNecessaryCurrency(orders[i]['total'], 1) + "</td>";
+        outPutTable+="<td>" + hideCardNumber(orders[i]['card_number']) + "</td>";
+        outPutTable+="<td>" + orders[i]['card_type'] + "</td>";
+        outPutTable+="<td>" + orders[i]['order_country'] + " " + "(" + orders[i]['order_ip'] + ")" + "</td>";
         
         outPutTable+="</tr>";
     }
